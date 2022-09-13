91f5a998602b72b6a3b048cfb0af8993916ce0d5
Faith Kemunto <faithkemunto98@gmail.com>
2022-09-12 19:34:47 -0700

    Puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  *main - Entry point
@@ -14,7 +13,7 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
 
